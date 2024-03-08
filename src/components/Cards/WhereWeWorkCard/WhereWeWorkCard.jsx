c309a2305f9d0e388a899b252fc826c088ffb435
Saksham <saksham@pearlorganisation.com>
2024-03-08 17:50:21 +0530

    added the 4 items per row

--- src/components/Cards/WhereWeWorkCard/WhereWeWorkCard.jsx
+++ src/components/Cards/WhereWeWorkCard/WhereWeWorkCard.jsx
@@ -5,21 +5,19 @@ import DummyImage from "../../../assets/natImg.jpg";
 
 const WhereWeWorkCard = ({ image, title, desc, members }) => {
   return (
-    <div>
-      <div
-        class="flex flex-col justify-between w-72 sm:w-96 h-96 bg-white bg-center bg-cover text-gray-800 shadow-md overflow-hidden cursor-pointer"
-        style={{ backgroundImage: `url(${image})` }}
-      >
-        <div class="flex justify-between items-center ml-4 pr-8">
-          <div class="bg-red-600 text-white bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded">
-            {desc}
-          </div>
-          <div class="bg-red-600 w-10 h-12 shadow flex flex-col-reverse p-2 text-center font-bold text-white rounded-b-full"></div>
+    <div
+      class="flex flex-col justify-between w-72 sm:w-72 h-72 bg-white bg-center bg-cover text-gray-800 shadow-md overflow-hidden cursor-pointer"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <div class="flex justify-between items-center ml-4 pr-8">
+        <div class="bg-red-600 text-white bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded">
+          {desc}
         </div>
-        <div class="bg-white bg-opacity-95 shadow-md rounded-r-xl p-4 flex flex-col mr-4 mb-8">
-          <h3 class="text-xl font-bold pb-2">{title}</h3>
-          <p class="truncate  text-md text-black">({members} Members)</p>
-        </div>
+        <div class="bg-red-600 w-10 h-12 shadow flex flex-col-reverse p-2 text-center font-bold text-white rounded-b-full"></div>
+      </div>
+      <div class="bg-white bg-opacity-95 shadow-md rounded-r-xl p-1 flex flex-col mr-4 mb-8">
+        <h3 class="text-lg font-bold pb-2">{title}</h3>
+        <p class="truncate  text-md text-black">({members} Members)</p>
       </div>
     </div>
   );
