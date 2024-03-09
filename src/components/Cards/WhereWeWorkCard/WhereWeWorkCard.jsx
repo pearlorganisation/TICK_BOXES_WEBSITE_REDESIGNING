aaae203ce5696160c7fe7d7f4bbfe9053869fd82
Saksham <saksham@pearlorganisation.com>
2024-03-09 10:23:38 +0530

    fixed the image issue

--- src/components/Cards/WhereWeWorkCard/WhereWeWorkCard.jsx
+++ src/components/Cards/WhereWeWorkCard/WhereWeWorkCard.jsx
@@ -6,18 +6,18 @@ import DummyImage from "../../../assets/natImg.jpg";
 const WhereWeWorkCard = ({ image, title, desc, members }) => {
   return (
     <div
-      class="flex flex-col justify-between w-72 sm:w-72 h-72 bg-white bg-center bg-cover text-gray-800 shadow-md overflow-hidden cursor-pointer"
+      className="flex flex-col justify-between w-72 sm:w-72 h-72 bg-white bg-center bg-cover text-gray-800 shadow-md overflow-hidden cursor-pointer"
       style={{ backgroundImage: `url(${image})` }}
     >
-      <div class="flex justify-between items-center ml-4 pr-8">
-        <div class="bg-red-600 text-white bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded">
+      <div className="flex justify-between items-center ml-4 pr-8">
+        <div className="bg-red-600 text-white bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded">
           {desc}
         </div>
-        <div class="bg-red-600 w-10 h-12 shadow flex flex-col-reverse p-2 text-center font-bold text-white rounded-b-full"></div>
+        <div className="bg-red-600 w-10 h-12 shadow flex flex-col-reverse p-2 text-center font-bold text-white rounded-b-full"></div>
       </div>
-      <div class="bg-white bg-opacity-95 shadow-md rounded-r-xl p-1 flex flex-col mr-4 mb-8">
-        <h3 class="text-lg font-bold pb-2">{title}</h3>
-        <p class="truncate  text-md text-black">({members} Members)</p>
+      <div className="bg-white bg-opacity-95 shadow-md rounded-r-xl p-1 flex flex-col mr-4 mb-8">
+        <h3 className="text-lg font-bold pb-2">{title}</h3>
+        <p className="truncate  text-md text-black">({members} Members)</p>
       </div>
     </div>
   );
